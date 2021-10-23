db2b7fa3fc3311f9f9db5ce1b96c23ca7b0c80de
Zolotarova-Nataliia <zolotarova.nataliia@gmail.com>
2021-10-23 15:51:23 +0300

    hw7 is done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+let modal;
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+  if (event.target.tagName === "IMG") {
+    modal = basicLightbox.create(
+      `
+            <img
+              width="1200"
+              height="900"
+              src="${event.target.dataset.source}" 
+              alt="${event.target.alt}">
+        `,
+      {
+        onShow: () => {
+          document.addEventListener("keydown", modalClose);
+        },
+        onClose: () => {
+          document.removeEventListener("keydown", modalClose);
+        },
+      }
+    );
+    modal.show();
+  }
+  console.log(event.target);
+}
+function modalClose(event) {
+  if (event.key === "Escape") {
+    modal.close();
+  }
+}
